80a467edf5cb8e6d5014dfb9e3c1089b7df76bb6
Ryo Fujimura <ryo176999@gmail.com>
2024-10-24 14:06:02 -0700

    ChopforBites project init

---
+++ ChopforBites/ChopforBitesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChopforBitesApp.swift
+//  ChopforBites
+//
+//  Created by ryo fujimura on 2024/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ChopforBitesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChopforBites/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChopforBites
+//
+//  Created by ryo fujimura on 2024/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
